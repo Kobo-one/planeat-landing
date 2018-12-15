afe61d7f2888c3208ac81b5bf3e1b7fbd6f71fdf
akasake <pham_see@hotmail.com>
2018-12-15 22:25:11 +0100

    added js

---
+++ public/javascript/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+            
+    $(".hamburger").click(function(){
+                $(".hamburger").toggleClass(" active");
+                $(".navlinks").toggleClass("hidemobile");
+    });
+});     
+    
