c850ccfe5b839f3859adfbbd6761c1305cf0a3d7
akasake <pham_see@hotmail.com>
2019-06-16 20:18:30 +0200

    optimalising with lazy load and remove animation when not seen

--- javascript/main.js
+++ javascript/main.js
@@ -1,20 +1,67 @@
-$(document).ready(function(){
-            
-    $(".hamburger").click(function(){
-                $(".hamburger").toggleClass("active");
-                $(".navlinks").toggleClass("hidemobile");
+document.addEventListener("DOMContentLoaded", function() {
+    var lazyloadImages = document.querySelectorAll("img.lazy");    
+    var lazyloadThrottleTimeout;
+    
+    function lazyload () {
+      if(lazyloadThrottleTimeout) {
+        clearTimeout(lazyloadThrottleTimeout);
+      }    
+      
+      lazyloadThrottleTimeout = setTimeout(function() {
+          var scrollTop = window.pageYOffset;
+          lazyloadImages.forEach(function(img) {
+              if(img.offsetTop < (window.innerHeight + scrollTop)) {
+                img.src = img.dataset.src;
+                img.classList.remove('lazy');
+              }
+          });
+          if(lazyloadImages.length == 0) { 
+            document.removeEventListener("scroll", lazyload);
+            window.removeEventListener("resize", lazyload);
+            window.removeEventListener("orientationChange", lazyload);
+          }
+      }, 20);
+    }
+    
+    document.addEventListener("scroll", lazyload);
+    window.addEventListener("resize", lazyload);
+    window.addEventListener("orientationChange", lazyload);
+  });
+
+  (function($) {
+
+    /**
+     * Copyright 2012, Digital Fusion
+     * Licensed under the MIT license.
+     * http://teamdf.com/jquery-plugins/license/
+     */
+
+    $.fn.visible = function(partial) {
+      
+        var $t            = $(this),
+            $w            = $(window),
+            viewTop       = $w.scrollTop(),
+            viewBottom    = viewTop + $w.height(),
+            _top          = $t.offset().top,
+            _bottom       = _top + $t.height(),
+            compareTop    = partial === true ? _bottom : _top,
+            compareBottom = partial === true ? _top : _bottom;
+      
+      return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+  
+    };
+      
+  })(jQuery);
+  
+  $(window).scroll(function(event) {
+    
+    $(".cloud").each(function(i, el) {
+      var el = $(el);
+      if (el.visible(true)) {
+        el.addClass("animate"); 
+      } else {
+        el.removeClass("animate");
+      }
     });
     
-}); 
-
-$(".item1").click(function(e){
-    e.preventDefault();
-    $(".feature__item--button1").toggleClass("hidden");
-    $(".feature__item--hidden1").toggleClass("hidden");
-});
-$(".item2").click(function(e){
-    e.preventDefault();
-    $(".feature__item--button2").toggleClass("hidden");
-    $(".feature__item--hidden2").toggleClass("hidden");
-});
-    
+  });
